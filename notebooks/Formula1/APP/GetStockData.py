1e2ef14e5cf3864df5f51dd6d785f35002906f58
vmlucas <64113156+vmlucas@users.noreply.github.com>
2022-09-12 23:01:54 -0300

    v20220912

--- notebooks/Formula1/APP/GetStockData.py
+++ notebooks/Formula1/APP/GetStockData.py
@@ -1,35 +1,10 @@
 # Databricks notebook source
 import urllib.request
 from io import StringIO
-import pymssql
 from pyspark.sql.functions import lit
 from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType, StringType, DateType
 
 
-
-# COMMAND ----------
-
-def insertData(cursor,stocks_df):
-  name = ''
-  stocks_entries = []   
-  cont=1  
-  dataCollect = stocks_df.collect()    
-  for row in dataCollect:  
-    name = row['stockname']
-    if( cont == 1):
-       cursor.execute('delete from stocks where stockname=%s',(name))
-       conn.commit()
-    stocks_entries.append((row['stockname'],row['date'],row['open'],row['high'],row['low'],row['close'],row['adj_close'],row['volume'])) 
-    if( cont == 1000):
-       cursor.executemany( "INSERT INTO stocks VALUES (%s, %s, %d,%d,%d,%d,%d,%d)",stocks_entries)
-       conn.commit()
-       cont =1
-       stocks_entries = []
-    cont = cont + 1
-
-  cursor.executemany( "INSERT INTO stocks VALUES (%s, %s, %d,%d,%d,%d,%d,%d)",stocks_entries)
-  conn.commit()
-  print(name," stocks inserted")
 
 # COMMAND ----------
 
@@ -51,8 +26,10 @@ def insertDataParquet(stocks_df):
 
 # COMMAND ----------
 
-def loadDataFromYahooFinance(cursor,nome):
-  url = "https://query1.finance.yahoo.com/v7/finance/download/"+nome+"?period1=-2208988800&period2=1662398916&interval=1d&events=history&crumb=fPlRrNhrzGa"
+import time 
+
+def loadDataFromYahooFinance(nome,init):
+  url = "https://query1.finance.yahoo.com/v7/finance/download/"+nome+"?period1="+init+"&period2="+str(int(time.time()))+"&interval=1d&events=history"
   response = urllib.request.urlopen(url)
   data = response.read()      
   text = data.decode('utf-8')  
@@ -67,7 +44,6 @@ def loadDataFromYahooFinance(cursor,nome):
                .schema(getStockSchema())\
                .csv("/mnt/victormrlucasformula1dl/app/stocks.csv")
   stocks_df = stocks_df.withColumn("stockname", lit(nome))
-  #insertData(cursor,stocks_df)
   insertDataParquet(stocks_df)  
   return nome+' stock loaded' 
 #stocks_df.printSchema()
@@ -75,32 +51,19 @@ def loadDataFromYahooFinance(cursor,nome):
 
 # COMMAND ----------
 
-#using azure sql server insert
-#db_server = 'vmlucas.database.windows.net'
-#db_user = dbutils.secrets.get(scope="formula1-scope",key="SQL-login")
-#db_pass = dbutils.secrets.get(scope="formula1-scope",key="sql-pass")
-#db_name = 'DB-CASA'
-
-#conn = pymssql.connect(db_server, db_user, db_pass, db_name)
-#cursor = conn.cursor()
-
 #using parquet file
-cursor = ''
-stocks = ['AMD', 'AAPL','NVDA','F','AMZN','TSLA','SNAP','NIO','BAC','CCL','ITUB','T','INTC','NOK','VALE','SHOP','SOFI','SWN','AUR','AMC','NU','AAL','DNA','PBR','PLTR']
-emptyRDD = spark.sparkContext.emptyRDD()
-df = spark.createDataFrame(emptyRDD,getStockSchema())
-df.write.mode('overwrite').parquet("/mnt/victormrlucasformula1dl/app/stocks.parquet")
+stocks = ['AMD', 'AAPL','NVDA','F','AMZN','TSLA','SNAP','NIO','DIS','GOOGL','ARKK','ADM','STOR','NEM','O','SHOP','ABNB','RIVN','AUR','AMC','NU','AAL','DNA','PBR','PLTR']
+#emptyRDD = spark.sparkContext.emptyRDD()
+#df = spark.createDataFrame(emptyRDD,getStockSchema())
+#df.write.mode('overwrite').parquet("/mnt/victormrlucasformula1dl/app/stocks.parquet")
+global init
+spark.sql("CREATE or replace TEMPORARY VIEW STOCKS USING parquet OPTIONS (path \"/mnt/victormrlucasformula1dl/app/stocks.parquet\")")  
+df = spark.sql("SELECT max(unix_timestamp(date, 'yyyy-MM-dd')) as data FROM STOCKS") 
+for row in df.collect():
+  init = str(row["data"])
+    
 for s in stocks:    
-  print(loadDataFromYahooFinance(cursor,s))
+  print(loadDataFromYahooFinance(s,init))
 
 #cursor.close()
 #conn.close()
-
-# COMMAND ----------
-
-spark.sql("CREATE or replace TEMPORARY VIEW STOCKS USING parquet OPTIONS (path \"/mnt/victormrlucasformula1dl/app/stocks.parquet\")")
-
-# COMMAND ----------
-
-spark.sql("SELECT max(date) FROM STOCKS where stockname='NVDA'").show()
-display(spark.sql("SELECT * FROM STOCKS where stockname='NVDA' and date > '2022-01-01'"))
